c14cfd7e3893b415cb5ffdb6315fe0aa2eec8055
Surya Vardhan <surya_24@live.com>
2026-08-25 22:46:37 +0530

    Initial Commit

---
+++ CarStatsWidget/CarStatsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarStatsWidgetApp.swift
+//  CarStatsWidget
+//
+//  Created by Surya Vardhan on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CarStatsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
